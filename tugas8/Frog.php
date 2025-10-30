b636b5dac9f24d2125ad49659338f4b352b1768e
Reza Hilmi Dafa <rezahilmidafa@gmail.com>
2025-10-30 13:41:55 +0700

    tugas 8 OOP PHP - Reza Hilmi Dafa

---
+++ tugas8/Frog.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'animal.php';
+
+class Frog extends Animal
+{
+  public function __construct($name)
+  {
+    parent::__construct($name);
+  }
+
+  public function jump()
+  {
+    return "hop hop";
+  }
+}
---
+++ tugas8/animal.php
@@ -0,0 +1,27 @@
+<?php
+class Animal
+{
+  private $name;
+  protected $legs;
+  private $cold_blooded;
+
+  public function __construct($name)
+  {
+    $this->name = $name;
+    $this->legs = 4;
+    $this->cold_blooded = "no";
+  }
+
+  public function get_name()
+  {
+    return $this->name;
+  }
+  public function get_legs()
+  {
+    return $this->legs;
+  }
+  public function get_cold_blooded()
+  {
+    return $this->cold_blooded;
+  }
+}
